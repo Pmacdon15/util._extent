9ac7a90403893bed02a097127b5c1b67fa2701d4
Patrick MacDonald <patrick@patmac.ca>
2026-02-11 15:11:28 -0700

    fc

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  cacheComponents: true,
+  async rewrites() {
+    return [{ source: "/force-error", destination: "https://google.com" }];
+  },
 };
 
 export default nextConfig;
